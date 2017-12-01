6fbc3f2e908152042886a90ba660634ab2aa5821
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-12-01 22:57:55 +0100

    Implement empty list TextView

--- app/src/main/java/com/spykins/locationtracker/ViewGeoListActivity.java
+++ app/src/main/java/com/spykins/locationtracker/ViewGeoListActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.spykins.locationtracker.db.DbManager;
 import com.spykins.locationtracker.ui.GeoRecyclerViewAdapter;
@@ -13,6 +15,7 @@ public class ViewGeoListActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private GeoRecyclerViewAdapter mGeoRecyclerViewAdapter;
     private DbManager mDbManager;
+    private TextView mEmptyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +29,14 @@ public class ViewGeoListActivity extends AppCompatActivity {
 
         mGeoRecyclerViewAdapter = new GeoRecyclerViewAdapter();
         mRecyclerView = findViewById(R.id.recycleView);
+        mEmptyTextView = findViewById(R.id.emptyList);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
         mRecyclerView.setAdapter(mGeoRecyclerViewAdapter);
         mGeoRecyclerViewAdapter.setGeoDataItems(mDbManager.getGeoDatabase().geoDataDao().fetchAllGeodata());
+        if (mGeoRecyclerViewAdapter.getItemCount() == 0) {
+            mEmptyTextView.setVisibility(View.VISIBLE);
+        }
 
     }
 
