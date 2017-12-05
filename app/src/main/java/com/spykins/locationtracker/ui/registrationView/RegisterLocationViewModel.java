7d4eea1c267a25b62f04b7c32a070565ab0fd028
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2017-12-05 13:02:00 +0100

    Fix permission issue with the App

--- app/src/main/java/com/spykins/locationtracker/ui/registrationView/RegisterLocationViewModel.java
+++ app/src/main/java/com/spykins/locationtracker/ui/registrationView/RegisterLocationViewModel.java
@@ -1,11 +1,14 @@
 package com.spykins.locationtracker.ui.registrationView;
 
+import static android.content.Context.LOCATION_SERVICE;
+
+import android.app.Activity;
 import android.app.PendingIntent;
 import android.arch.lifecycle.LifecycleOwner;
-import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Observer;
 import android.content.Context;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -23,14 +26,17 @@ import java.util.Calendar;
 public class RegisterLocationViewModel extends android.arch.lifecycle.ViewModel implements
         RegistrationContract.ViewModel {
 
+    private final String TAG = RegisterLocationViewModel.class.getSimpleName();
     private  WeakReference<RegistrationContract.View> mView;
     private Util mUtil;
     GeoFenceReceiver mGeoFenceReceiver = new GeoFenceReceiver();
     private AppManager mAppManager;
+    private boolean isLocationSettingsOn;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mShouldSetUpGeoFencing;
 
 
-    public RegisterLocationViewModel() {
-    }
+    public RegisterLocationViewModel() {}
 
     @Override
     public void setView(RegistrationContract.View view, Util util) {
@@ -41,44 +47,49 @@ public class RegisterLocationViewModel extends android.arch.lifecycle.ViewModel 
     @Override
     public boolean shouldProceedWithRegisteration(String addressText, String longitudeText, String latitudeText) {
         boolean isValidDouble = mUtil.isValidDouble(longitudeText) && mUtil.isValidDouble(latitudeText);
+        boolean flag = true;
+        try {
+            RegistrationContract.View view = mView.get();
+            if (view != null) {
+                if (addressText.isEmpty()) {
+                    flag = false;
+                    view.setErrorTextViewForAddressText();
+                    return flag;
+                }
 
-        if (addressText.isEmpty()) {
-            if (mView.get() != null) mView.get().setErrorTextViewForAddressText();
-            return false;
+                if (!isValidDouble) {
+                    flag = false;
+                    view.setErrorTextViewForLocation();
+                    return flag;
+                }
+            }
+
+        } catch (NullPointerException e) {
+            Log.d(TAG, "The View is null");
         }
 
-        if (!isValidDouble) {
-            if (mView.get() != null) mView.get().setErrorTextViewForLocation();
-            return false;
-        }
-
-        return true;
+        return flag;
     }
 
-    @Override
     public void registerGoogleApiClient(final boolean shouldSetUpGeoFencing) {
-
-        GoogleApiClient googleApiClient = Injector.provideGoogleApiClient(new GoogleApiClient.ConnectionCallbacks() {
+        mShouldSetUpGeoFencing = shouldSetUpGeoFencing;
+        mGoogleApiClient = Injector.provideGoogleApiClient(new GoogleApiClient.ConnectionCallbacks() {
             @Override
             public void onConnected(@Nullable Bundle bundle) {
-                setUpGeoFencingInNeccessary(shouldSetUpGeoFencing);
+                setUpGeoFencingInNeccessary();
             }
 
             @Override
             public void onConnectionSuspended(int i) {}
         });
 
-        if (googleApiClient.isConnected()) {
-            setUpGeoFencingInNeccessary(shouldSetUpGeoFencing);
-        } else {
-            googleApiClient.connect();
-        }
+        checkIfPermissionIsOn();
     }
 
-    private void setUpGeoFencingInNeccessary(boolean shouldSetUpGeoFencing) {
+    private void setUpGeoFencingInNeccessary() {
         Log.d("wale", "log onConnected");
         mAppManager = Injector.provideAppManager();
-        if (shouldSetUpGeoFencing) {
+        if (mShouldSetUpGeoFencing) {
             setUpGeoFencing();
         } else {
             fetchCurrentLocation();
@@ -86,43 +97,119 @@ public class RegisterLocationViewModel extends android.arch.lifecycle.ViewModel 
     }
 
     private void setUpGeoFencing() {
+        RegistrationContract.View view = mView.get();
 
-        if (mView.get() != null) mView.get().registerGeoFenceReceiver(mGeoFenceReceiver);
+        try {
+            if (view != null) {
+                view.registerGeoFenceReceiver(mGeoFenceReceiver);
 
-        Calendar calendar = Calendar.getInstance();
-        String longitude = mView.get() != null ? mView.get().fetchLongitudeValue() : "";
-        String latitude = mView.get() != null ? mView.get().fetchLatitudeValue() : "";
-        String address = mView.get() != null ? mView.get().fetchAddressValue() : "";
+                Calendar calendar = Calendar.getInstance();
+                String longitude = view.fetchLongitudeValue();
+                String latitude = view.fetchLatitudeValue();
+                String address = view.fetchAddressValue();
 
-        GeoData geoData;
-        if (mUtil.isValidDouble(latitude) && mUtil.isValidDouble(longitude) && !address.isEmpty() ) {
-            geoData = new GeoData(calendar.getTime(), Double.valueOf(latitude),
-                    Double.valueOf(longitude), address);
-        } else {
-            if (mView.get() != null) mView.get().setErrorUnKnown();
-            return;
+                GeoData geoData;
+                if (mUtil.isValidDouble(latitude) && mUtil.isValidDouble(longitude) && !address.isEmpty() ) {
+                    geoData = new GeoData(calendar.getTime(), Double.valueOf(latitude),
+                            Double.valueOf(longitude), address);
+                } else {
+                    view.setErrorUnKnown();
+                    return;
+                }
+
+                PendingIntent pendingIntent = view.getPendingIntent();
+                Context context = (Context) view;
+                mAppManager.init(geoData, pendingIntent,context );
+                view.finishActivity();
+            }
+
+        } catch (NullPointerException e) {
+            Log.d(TAG, "The View is null");
         }
-
-        PendingIntent pendingIntent = mView.get() != null ? mView.get().getPendingIntent() : null;
-        Context context = mView.get() != null ? (Context) mView.get() : null;
-        mAppManager.init(geoData, pendingIntent,context );
-        if (mView.get() != null) mView.get().finishActivity();
     }
 
     private void fetchCurrentLocation() {
-        Context context =  mView.get() != null ? (Context)mView.get() : null;
 
-        mAppManager.fetchCurrentUserLocation(context).observe((LifecycleOwner) context, new Observer<Location>() {
-            @Override
-            public void onChanged(@Nullable Location location) {
-                if (location == null) {
-                    if (mView.get() != null) mView.get().setErrorInLocation();
-                } else {
-                    if (mView.get() != null) mView.get().setLocationValueOnView(location);
-                }
+        final RegistrationContract.View view = mView.get();
+
+        try {
+            if (view != null) {
+                Context context =  (Context)view;
+
+                mAppManager.fetchCurrentUserLocation(context).observe((LifecycleOwner) context, new Observer<Location>() {
+                    @Override
+                    public void onChanged(@Nullable Location location) {
+                        if (location == null) {
+                            view.setErrorInLocation();
+                        } else {
+                            view.setLocationValueOnView(location);
+                        }
+                    }
+                });
             }
-        });
+
+        } catch (NullPointerException e) {
+            Log.d(TAG, "The View is null");
+
+        }
+    }
+
+    public void showLocationSettingsIfLocationIsOff() {
+        RegistrationContract.View view = mView.get();
+        if (view != null &&  !hasEnabledDeviceGps()) {
+            view.startSettingsActivity();
+            isLocationSettingsOn = false;
+        } else {
+            isLocationSettingsOn = true;
+        }
+        checkIfConditionToConnectIsSatisfied();
+    }
+
+    private void checkIfConditionToConnectIsSatisfied() {
+        if (isLocationSettingsOn && isLocationPermissionOn()) {
+            Log.d(TAG,"LocationSetting is on and permission is on");
+            setUpApp();
+            //Allow Application
+        }
+    }
+
+    private void setUpApp() {
+        if (mGoogleApiClient.isConnected()) {
+            setUpGeoFencingInNeccessary();
+        } else {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    private boolean isLocationPermissionOn() {
+        RegistrationContract.View view = mView.get();
+        if (view != null) {
+            return view.isFineLocationPermissionGranted();
+        }
+        return false;
     }
 
 
+    private void checkIfPermissionIsOn() {
+
+        RegistrationContract.View view = mView.get();
+
+        if (view != null) {
+            if (!view.isFineLocationPermissionGranted()) {
+                view.requestForPermission();
+            } else {
+                showLocationSettingsIfLocationIsOff();
+            }
+        }
+    }
+
+
+    public boolean hasEnabledDeviceGps() {
+        RegistrationContract.View view = mView.get();
+        if (view != null) {
+            LocationManager locationManager = (LocationManager) ((Activity) view).getSystemService(LOCATION_SERVICE);
+            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }
+        return false;
+    }
 }
